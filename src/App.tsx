d1d7c3f692b8f9e844d18b11530a7de21fb8cd2a
Santiago Yanguas <santitoya-2001@outlook.com>
2021-12-16 22:52:22 -0500

    [Feature] Add styles, logo, del import bootstrap, develop the home

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./styles/style.css";
+import Logo from "./img/logo.png";
+import { response } from "./fixture";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <div className="header__imgtype">
+          <div className="header__boxLogo">
+            <img className="header__logo" src={Logo} alt="spiderman" />
+          </div>
+          <p className="header__title">SuperHero API</p>
+        </div>
+        <nav>
+          <ul className="nav-list">
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">My Team</a>
+            </li>
+          </ul>
+          `
+        </nav>
       </header>
+      <main>
+        <h1>Build Your Team</h1>
+        <div className="search">
+          <input
+            className="search__input"
+            type="text"
+            placeholder="Search your SuperHero"
+          />
+          <button className="article__learn-more cursor-pointer search__icon">
+            🔎
+          </button>
+        </div>
+        <div className="articles">
+          <article className="article">
+            <h2>Lorem Ipsum</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+              feugiat augue sit amet posuere lobortis. Cras erat risus, pulvinar
+              eget augue id, molestie ultricies lectus. Curabitur et aliquam
+            </p>
+            <button className="article__learn-more btn-mt">➕Add</button>
+          </article>
+          <article className="article">
+            <h2>Lorem Ipsum</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+              feugiat augue sit amet posuere lobortis. Cras erat risus, pulvinar
+              eget augue id, molestie ultricies lectus. Curabitur et aliquam
+            </p>
+            <button className="article__learn-more btn-mt">➕Add</button>
+          </article>
+          <article className="article">
+            <h2>Lorem Ipsum</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+              feugiat augue sit amet posuere lobortis. Cras erat risus, pulvinar
+              eget augue id, molestie ultricies lectus. Curabitur et aliquam
+            </p>
+            <button className="article__learn-more btn-mt">➕Add</button>
+          </article>
+        </div>
+      </main>
     </div>
   );
 }
