4457f3b8e5b1d5446faadc16121906a60c5707fb
Santiago Yanguas <santitoya-2001@outlook.com>
2021-12-18 22:25:24 -0500

    [Feature] Update team with filters and cards

--- src/components/items/Cards.tsx
+++ src/components/items/Cards.tsx
@@ -3,11 +3,14 @@ import { Card } from "./Card";
 export const Cards = ({ response, edit = false }: any) => {
   console.log("response: ", typeof response);
   return (
-    <div className="articles">
-      {response &&
-        response.map((item: any) => (
-          <Card edit={edit} key={item.id} item={item} />
-        ))}
-    </div>
+    <>
+      {response && <h4>Results: ({response.length})</h4>}
+      <div className="articles">
+        {response &&
+          response.map((item: any) => (
+            <Card edit={edit} key={item.id} item={item} />
+          ))}
+      </div>
+    </>
   );
 };
--- src/pages/Team.tsx
+++ src/pages/Team.tsx
@@ -1,15 +1,45 @@
 import { useSelector } from "react-redux";
 import { RootState } from "../redux/store";
 import { Cards } from "../components/items/Cards";
+import { Form } from "../components/forms/Form";
+import { useEffect, useState } from "react";
+import { SuperHero } from "../interfaces/superheroes";
 
 export const Team = () => {
-  const { teamHeroes } = useSelector((state: RootState) => state.heroesRed);
-  console.log("item: ", teamHeroes);
+  const { teamHeroes } =
+    useSelector((state: RootState) => state.heroesRed) || [];
+  const [heroes, setHeroes] = useState<SuperHero[]>([]);
+  const [restart, setRestart] = useState(false);
+
+  const changeHeroes = (heroesUpdated: any) => {
+    setHeroes(heroesUpdated);
+  };
+
+  const resetHeroes = () => {
+    setRestart(!restart);
+  };
+
+  useEffect(() => {
+    setHeroes(teamHeroes);
+  }, [teamHeroes, restart]);
 
   return (
     <main>
-      <h1>Equipo</h1>
-      <Cards edit={true} response={teamHeroes} />
+      {heroes.length > 0 ? (
+        <>
+          <h1>Equipo</h1>
+          <br />
+          <h2>Filtros</h2>
+          <Form
+            heroes={heroes}
+            changeHeroes={changeHeroes}
+            resetHeroes={resetHeroes}
+          />
+          <Cards edit={true} response={heroes} />
+        </>
+      ) : (
+        <h1>Crea tu equipo primero</h1>
+      )}
     </main>
   );
 };
